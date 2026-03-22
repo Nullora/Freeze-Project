1bbea41d4c9e5f9f7e6b955085a40c83f1e7a653
Clashnewbme <amoryjamesmonson@icloud.com>
2026-03-22 20:00:54 +0800

    Update kernel.c

--- FreezeProject/src/kernel.c
+++ FreezeProject/src/kernel.c
@@ -16,7 +16,20 @@ const unsigned int multiboot_header[] = {
 // ENTRY 
 void kernel_main(void){
     clear();
-    print("\033[96m=== \033[95mFreeze Project\033[96m ===\033[0m\n");
+    print("\033[1;36m");
+
+    print("___________                                    \n");
+    print("\\_   _____/______   ____   ____ ________ ____  \n");
+
+    print("\033[1;32m"); // Bright green
+    print(" |    __) \\_  __ \\_/ __ \\_/ __ \\\\___   // __ \\ \n");
+    print(" |     \\   |  | \\/\\  ___/\\  ___/ /    /\\  ___/ \n");
+
+    print("\033[1;36m"); // Back to cyan
+    print(" \\___  /   |__|    \\___  >\\___  >_____ \\\\___  >\n");
+    print("     \\/                \\/     \\/      \\/    \\/ \n");
+
+    print("\033[0m");
     print("\033[92mhttps://freezeos.org/\033[0m\n");
     print("\033[93mType 'help' for help on learning commands\033[0m\n\n");
     print("\033[94m--------------------------------\033[0m\n");
