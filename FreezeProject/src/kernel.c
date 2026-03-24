6c30a9b5029d62d852d1ccca9ff698bddd92be0d
Clashnewbme <amoryjamesmonson@icloud.com>
2026-03-24 13:23:12 +0800

    Update kernel.c

--- FreezeProject/src/kernel.c
+++ FreezeProject/src/kernel.c
@@ -13,7 +13,60 @@ const unsigned int multiboot_header[] = {
 #include "shell.h"
 #include <stdint.h>
 
-// ENTRY 
+void print_hex(uint32_t val);
+
+
+static inline void outl(uint16_t port, uint32_t val) {
+    __asm__ volatile ("outl %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint32_t inl(uint16_t port) {
+    uint32_t ret;
+    __asm__ volatile ("inl %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+
+uint32_t pci_read(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
+    uint32_t address = (uint32_t)((bus << 16) | (slot << 11) |
+                                 (func << 8) | (offset & 0xFC) |
+                                 ((uint32_t)0x80000000));
+
+    outl(0xCF8, address);
+    return inl(0xCFC);
+}
+
+
+void pci_scan() {
+    print("\n[PCI] Scanning...\n");
+
+    for (uint16_t bus = 0; bus < 256; bus++) {
+        for (uint8_t slot = 0; slot < 32; slot++) {
+
+            uint32_t data = pci_read(bus, slot, 0, 0);
+            uint16_t vendor = data & 0xFFFF;
+
+            if (vendor == 0xFFFF || vendor == 0x0000)
+                continue;
+
+            uint16_t device = (data >> 16) & 0xFFFF;
+
+            print("[PCI] ");
+            print_hex(vendor);
+            print(":");
+            print_hex(device);
+            print("\n");
+
+            if (vendor == 0x8086 && device == 0x100E) {
+                print("\n[NET] Found e1000!\n");
+                return;
+            }
+        }
+    }
+
+    print("\n[NET] No e1000 found.\n");
+}
+// Entry
 void kernel_main(void){
     clear();
 
@@ -23,10 +76,13 @@ void kernel_main(void){
     print(" |     \\   |  | \\/\\  ___/\\  ___/ /    /\\  ___/ \n");
     print(" \\___  /   |__|    \\___  >\\___  >_____ \\\\___  >\n");
     print("     \\/                \\/     \\/      \\/    \\/ \n");
+
     print("\033[92mhttps://freezeos.org/\033[0m\n");
     print("\033[93mType 'help' for help on learning commands\033[0m\n\n");
     print("\033[94m--------------------------------\033[0m\n");
     print("\033[92mCurrently: \033[93mVersion 0.64\033[0m\n");
+
+    pci_scan(); 
 
     shell();
 }
